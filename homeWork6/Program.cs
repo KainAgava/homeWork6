a894e66f0abf20dcf307960410f2a9a6ce6ad886
KainAgava <143312166+KainAgava@users.noreply.github.com>
2023-08-28 15:29:59 +0700

    Add files via upload

---
+++ homeWork6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberGrandmothers;
+
+            Console.WriteLine("сколько в очереди бабушек? ");
+            numberGrandmothers = Convert.ToInt32(Console.ReadLine());
+            
+            int fixedReceptionTime = 10 * numberGrandmothers;
+            int minute = fixedReceptionTime % 60;
+            int hour = fixedReceptionTime / 60;
+            
+          
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hour} часа и {minute} минут.");
+
+        }
+    }
+}
